eb712cfe5e3607447ad56b8b78aa498717aa6ffd
Nikolozi Chikhladze <nikolozi_chikhladze@epam.com>
2023-03-08 13:42:02 +0400

    feat: readme upload

--- src/pages/favourites/Favorites.tsx
+++ src/pages/favourites/Favorites.tsx
@@ -3,15 +3,20 @@ import React, {useEffect} from 'react';
 import {FavoritesList} from '../../components/favoritesList/FavoritesList';
 import {FavoritesPageHeader} from '../../components/favoritesPageHeader/FavoritesPageHeader';
 import {useAppSelector} from '../../hooks/redux';
+import {RootNavigationParamList} from '../../navigation/navigation.constants';
 import {PreAuthPage} from '../authentication/preAuth/PreAuth';
 
 export const FavoritesPage = () => {
   const loggedIn = useAppSelector(state => state.user.loggedIn);
-  const {setOptions} = useNavigation();
+  const {setOptions} = useNavigation<RootNavigationParamList>();
 
   useEffect(() => {
     if (loggedIn === true) {
-      setOptions({headerShown: true, headerRight: FavoritesPageHeader});
+      setOptions({
+        headerShown: true,
+        headerRight: FavoritesPageHeader,
+        headerLeft: () => null,
+      });
     } else {
       setOptions({headerShown: false});
     }
